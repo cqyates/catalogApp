d2f5185ab2d81991a44902a9c52f114b018f85fc
Corey <cqyates@gmail.com>
2019-08-20 11:57:31 -0400

    added user to db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
+const gravatar = require('gravatar');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useCreateIndex: true
         });
         console.log('MongoDB Connected')
     } catch (err) {
