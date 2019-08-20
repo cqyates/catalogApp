6ee03dee837507cadab1f2c59f1e2a7a9e665127
Corey <cqyates@gmail.com>
2019-08-20 18:37:54 -0400

    added auth route

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/API/users'));
 app.use('/api/upload', require('./routes/API/upload'));
 app.use('/api/search', require('./routes/API/search'));
+app.use('/api/auth', require('./routes/API/auth'))
 
 const PORT = process.env.PORT || 5000;
 
