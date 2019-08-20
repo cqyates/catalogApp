27f903ea3246c5935a49d20b00a16a22e929d79f
Corey <cqyates@gmail.com>
2019-08-20 18:07:33 -0400

    added token

--- routes/API/users.js
+++ routes/API/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
@@ -52,10 +54,21 @@ router.post('/',
             
             await user.save();
 
-            //Return jsonwebtoken come back later
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-        
-            res.send('user registered');
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token })
+                }
+                )
         } catch(err) {
              console.error(err.message);
              res.status(500).send('Server Error')
